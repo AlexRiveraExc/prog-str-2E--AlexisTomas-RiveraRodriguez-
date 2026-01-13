0bd63ebcae3a3e7c39199606115eb1efc4b276cd
AlexRiveraExc <20243ds065@utez.edu.mx>
2026-01-13 09:37:48 -0600

    chore: base actividad git merge withoutconflict

---
+++ MergeWithOutConflic/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Nombre del alumno: Alexis Tomas Rivera Rodriguez
+ * Carrera: DSM
+ * Materia: Programación Estructurada
+ * Universidad: Universidad Emiliano Zapata
+ * Profesor: Eliel Rodriguez
+ * Cuatrimestre: 2
+ * Grupo: E
+ * Fecha: 13/01/2026
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Semana 2");
+        System.out.println("Version Base");
+    }
+}
