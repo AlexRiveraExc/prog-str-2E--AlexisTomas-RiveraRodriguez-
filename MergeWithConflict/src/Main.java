4332cab55b46a1340911f9435d8e0385abcfe736
AlexRiveraExc <20243ds065@utez.edu.mx>
2026-01-14 17:12:19 -0600

    base conflicto

---
+++ MergeWithConflict/src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Nombre del alumno: Alexis Tomas Rivera Rodriguez
+ * Carrera: DSM
+ * Materia: Programación Estructurada
+ * Universidad: Universidad Emiliano Zapata
+ * Profesor: Eliel
+ * Cuatrimestre: 2
+ * Grupo: E
+ * Fecha: 14/01/2026
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Mensaje original");
+
+    }
+
+}
