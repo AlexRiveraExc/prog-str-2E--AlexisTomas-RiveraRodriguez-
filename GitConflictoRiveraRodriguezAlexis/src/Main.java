5df328cc04badf7e1f79656e39fc384174f99ff7
AlexRiveraExc <20243ds065@utez.edu.mx>
2026-01-19 20:15:56 -0600

    fix: resolver conflicto y unificar logica

--- GitConflictoRiveraRodriguezAlexis/src/Main.java
+++ GitConflictoRiveraRodriguezAlexis/src/Main.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Main {
 
     public static final double IVA = 0.16;
+    public static final double DESCUENTO = 0.10;
+    public static final double UMBRAL_DESCUENTO = 1000.0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         double subtotal = pedirDouble(scanner, "Subtotal: ");
-        double total = subtotal + (subtotal * IVA);
+        double total = calcularTotalConIva(subtotal);
+        total = aplicarDescuentoSiAplica(total, subtotal);
 
         System.out.printf("Total a pagar: %.2f%n", total);
     }
@@ -16,5 +19,16 @@ public class Main {
     public static double pedirDouble(Scanner scanner, String mensaje) {
         System.out.print(mensaje);
         return scanner.nextDouble();
+    }
+
+    public static double calcularTotalConIva(double subtotal){
+        return subtotal + (subtotal * IVA);
+    }
+
+    public static double aplicarDescuentoSiAplica(double total, double subtotal){
+        if(subtotal > UMBRAL_DESCUENTO){
+            total = total - (total * DESCUENTO);
+        }
+        return total;
     }
 }
