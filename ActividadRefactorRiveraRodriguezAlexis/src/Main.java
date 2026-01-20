dfb19c23081fc301c8ed93b73244c120de9036cc
AlexRiveraExc <20243ds065@utez.edu.mx>
2026-01-19 19:34:40 -0600

    comentado de programa para buenas practicas

--- ActividadRefactorRiveraRodriguezAlexis/src/Main.java
+++ ActividadRefactorRiveraRodriguezAlexis/src/Main.java
@@ -1,3 +1,15 @@
+/**
+ * Nombre del alumno: Alexis Tomas Rivera Rodriguez
+ * Carrera: DSM
+ * Materia: Programación Estructurada
+ * Universidad: Universidad Emiliano Zapata
+ * Profesor: Eliel
+ * Cuatrimestre: 2
+ * Grupo: E
+ * Fecha: 19/01/2026
+ */
+
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,13 +18,18 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        // se piden los valores al usuario
         int a = pedirEntero(scanner, "a: ");
         int b = pedirEntero(scanner, "b: ");
         int c = pedirEntero(scanner, "c: ");
 
+        // calcula la suma
         int suma = calcularSuma(a, b, c);
+
+        // calcula el promedio
         double prom = calcularPromedio(suma);
 
+        // muestra los resultados
         System.out.println("suma=" + suma);
         System.out.println("prom=" + prom);
     }
@@ -22,10 +39,12 @@ public class Main {
         return scanner.nextInt();
     }
 
+    // metodo que suma tres numeros
     public static int calcularSuma(int a, int b, int c){
         return a + b + c;
     }
 
+    // metodo que calcula el promedio a partir de la suma
     public static double calcularPromedio(int suma){
         return suma / 3.0;
     }
